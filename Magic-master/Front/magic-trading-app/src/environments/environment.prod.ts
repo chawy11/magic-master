9a8ae96cd8768a549b6fd4a030738012b913c9c4
DESKTOP-TTAJS5G\carlo <carlos110393@gmail.com>
2025-05-29 14:30:51 +0200

    ultra new

--- Magic-master/Front/magic-trading-app/src/environments/environment.prod.ts
+++ Magic-master/Front/magic-trading-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   urlBase: 'https://api.scryfall.com',
-  apiUrl: 'https://magic-master-production.up.railway.app'
+  apiUrl: 'https://magic-master-production.up.railway.app/api'
 };
